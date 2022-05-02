6beec2a83a29290c2308b8bb71889a0377485134
qusewen <78975759+qusewen@users.noreply.github.com>
2022-05-02 12:47:48 +0300

    feat: added homework 3

--- task3.js
+++ task3.js
@@ -82,44 +82,127 @@ if (pinCode !== pin) {
   console.log("вход выполнен");
 }
 
-
-// 3. Создайте программу, которая выводит на экран (alert, console.log) данные для чисел 
-// произвольного диапазона (можно установить его самостоятельно) по аналогии с задачей 
-// 2 предыдущего домашнего задания (больше 100, четное/нечетное, 
+// 3. Создайте программу, которая выводит на экран (alert, console.log) данные для чисел
+// произвольного диапазона (можно установить его самостоятельно) по аналогии с задачей
+// 2 предыдущего домашнего задания (больше 100, четное/нечетное,
 // положительное/отрицательное).
 
-let firstNumScore = Math.ceil((Math.random() * (50 - (-50)) + (-50)));
-let secondNumScore = Math.ceil((Math.random() * (50 - (-50)) + (-50)));
-if(firstNumScore < secondNumScore){
-    for (firstNumScore; firstNumScore < secondNumScore; firstNumScore++){
-        if (((firstNumScore % 2) === 0) && (firstNumScore > 100) && (firstNumScore > 0)){
-        console.log(`${firstNumScore} - четное, больше 100, положительное` );
-    }else if(((firstNumScore % 2) === 0) && (firstNumScore > 100) && (firstNumScore < 0)){
-        console.log(`${firstNumScore} - четное, больше 100, отрицательное` );
-    }else if(((firstNumScore % 2) === 0) && (firstNumScore < 100) && (firstNumScore < 0)){
-        console.log(`${firstNumScore} - четное, меньше 100, отрицательное` );
-    }else if(((firstNumScore % 2) !== 0) && (firstNumScore < 100) && (firstNumScore < 0)){
-        console.log(`${firstNumScore} - нечетное, меньше 100, отрицательное` );
-    }else if(((firstNumScore % 2) !== 0) && (firstNumScore < 100) && (firstNumScore > 0)){
-        console.log(`${firstNumScore} - нечетное, меньше 100, Положительное` );
-    }else if(((firstNumScore % 2) !== 0) && (firstNumScore > 100) && (firstNumScore > 0)){
-        console.log(`${firstNumScore} - нечетное, больше 100, Положительное` );
-    }else if(((firstNumScore % 2) === 0) && (firstNumScore < 100) && (firstNumScore > 0)){
-        console.log(`${firstNumScore} - четное, меньше 100, Положительное` );
-    }else if(((firstNumScore % 2) !== 0) && (firstNumScore > 100) && (firstNumScore > 0)){
-        console.log(`${firstNumScore} - нечетное, больше 100, отрицательно` );
-    }else{
-
+let firstNumScore = Math.ceil(Math.random() * (50 - -50) + -50);
+let secondNumScore = Math.ceil(Math.random() * (50 - -50) + -50);
+if (firstNumScore < secondNumScore) {
+  for (firstNumScore; firstNumScore < secondNumScore; firstNumScore++) {
+    if (firstNumScore % 2 === 0 && firstNumScore > 100 && firstNumScore > 0) {
+      console.log(`${firstNumScore} - четное, больше 100, положительное`);
+    } else if (
+      firstNumScore % 2 === 0 &&
+      firstNumScore > 100 &&
+      firstNumScore < 0
+    ) {
+      console.log(`${firstNumScore} - четное, больше 100, отрицательное`);
+    } else if (
+      firstNumScore % 2 === 0 &&
+      firstNumScore < 100 &&
+      firstNumScore < 0
+    ) {
+      console.log(`${firstNumScore} - четное, меньше 100, отрицательное`);
+    } else if (
+      firstNumScore % 2 !== 0 &&
+      firstNumScore < 100 &&
+      firstNumScore < 0
+    ) {
+      console.log(`${firstNumScore} - нечетное, меньше 100, отрицательное`);
+    } else if (
+      firstNumScore % 2 !== 0 &&
+      firstNumScore < 100 &&
+      firstNumScore > 0
+    ) {
+      console.log(`${firstNumScore} - нечетное, меньше 100, Положительное`);
+    } else if (
+      firstNumScore % 2 !== 0 &&
+      firstNumScore > 100 &&
+      firstNumScore > 0
+    ) {
+      console.log(`${firstNumScore} - нечетное, больше 100, Положительное`);
+    } else if (
+      firstNumScore % 2 === 0 &&
+      firstNumScore < 100 &&
+      firstNumScore > 0
+    ) {
+      console.log(`${firstNumScore} - четное, меньше 100, Положительное`);
+    } else if (
+      firstNumScore % 2 !== 0 &&
+      firstNumScore > 100 &&
+      firstNumScore > 0
+    ) {
+      console.log(`${firstNumScore} - нечетное, больше 100, отрицательно`);
+    } else {
     }
-    
-    }
+  }
 }
 
-
-
-// 4.IV. Создайте цикл, который при любом диапазоне перебираемых в нем чисел, 
+// 4.IV. Создайте цикл, который при любом диапазоне перебираемых в нем чисел,
 // прерывался бы на седьмой итерации.
-
-for (let i = 1; i < 8; i++){
-    console.log(`итерация № ${i}`)
+let firstMin = -10;
+let secondMax = 10;
+for (let i = 1; i < 8; i++) {
+  console.log(`итерация № ${i}`);
+  for (firstMin; firstMin < secondMax; firstMin++) {
+    console.log(firstMin);
+  }
 }
+
+// function
+
+//1. Напишите функцию, которая принимает от одного до трех параметров, и возвращает
+//их сумму.
+
+function sumTherd(a, b, c) {
+  sum = a + b + c;
+  return sum;
+}
+console.log(sumTherd(2, 5, 4));
+
+//   2. Напишите функцию, которая в качестве параметров принимает три строчных значения
+// и возвращает сумму строк в алфавитном порядке, разделенных пробелом.
+// Предполагается, что все значения введены на одном языке.
+
+function str(str1, str2, str3) {
+  let n = [str1, str2, str3];
+  let m = n.sort();
+  let sum = m[0] + " " + m[1] + " " + m[2];
+
+  return sum;
+}
+console.log(str("ewr", "aser", "bert"));
+
+// 3. Напишите функцию, которая возвращает подпись к электронному письму в стиле "С
+// уважением, {Имя Пользователя}".
+// Имя пользователя передается в качестве параметра. Если этот параметр не передается,
+// функция должна запросить его у пользователя при помощи prompt.
+
+function mailName(username) {
+  if (username !== undefined) {
+    return `С уважением ${username}`;
+  } else if (username === undefined) {
+    let enterUserName = prompt("введите имя пользователя");
+    return `С уважением ${enterUserName}`;
+  }
+}
+console.log(mailName());
+
+// 4. Напишите функцию, которая принимает в качестве параметров два положительных
+// целых числа, и возвращает первое число возведенное в степень, равную второму
+// параметру. В случае, если второй параметр не передан, функция возвращает первый
+// параметр возведенный в квадрат
+
+function numberPow(num, num1) {
+  if (num1 > 0) {
+    return (num = num ** num1);
+  } else if (num1 === "") {
+    return num ** 2;
+  } else {
+    return "вы ввели не правльное значение";
+  }
+}
+
+console.log(numberPow(3, ""));
